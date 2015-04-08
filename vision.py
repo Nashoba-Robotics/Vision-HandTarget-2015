8cc750920ce840c92463ef46caaebd95fa2bc804
Colin Willoughby <colinw@savix.net>
2015-04-08 17:39:02 -0400

    Changed Target Type

--- vision.py
+++ vision.py
@@ -1,26 +1,72 @@
 import rr
 import math
 
-try: 
-    distances = rr.GetArrayVariable("FIDUCIAL_DISTANCE_ARRAY")
+try:
+    index = -1
+    names = rr.GetArrayVariable("FIDUCIAL_NAME_ARRAY")
+    for x in range(len(names)):
+        s = str(names[x])
+        if "arrow" in s:
+            index = x
+    if index != -1:
+        rr.SetVariable("InsideVisible", True)
+        distances = rr.GetArrayVariable("FIDUCIAL_DISTANCE_ARRAY")
+        x_positions = rr.GetArrayVariable("FIDUCIAL_X_COORD_ARRAY")
+        
+        #Output the horizontal displacement of the middle target
+        imageCenterX = rr.GetVariable("IMAGE_WIDTH")/2
+        targetX = x_positions[index]
+        centerDistance = distances[index]
+        rr.SetVariable("TargetDistance", centerDistance)
+        rr.SetVariable("TargetX", targetX - imageCenterX)
+        rr.SetVariable("TargetVisible", True)
+        
+        rightIndex = -1
+        names = rr.GetArrayVariable("FIDUCIAL_NAME_ARRAY")
+        for x in range(len(names)):
+            s = str(names[x])
+            if "right" in s:
+                rightIndex = x
+                
+        leftIndex = -1
+        names = rr.GetArrayVariable("FIDUCIAL_NAME_ARRAY")
+        for x in range(len(names)):
+            s = str(names[x])
+            if "left" in s:
+                leftIndex = x
+        if leftIndex != -1 and rightIndex != -1: 
+            r = distances[leftIndex]
+            l = distances[rightIndex]
 
-    r = distances[0]
-    l = distances[2]
-    cam = 14.125
+            rr.SetVariable("r", r)
+            rr.SetVariable("l", l)
+            
+            cam = 14.125
 
-    L = math.acos((-(l**2)+cam**2+r**2)/(2*cam*r))
-    R = math.acos((l**2+cam**2-r**2)/(2*l*cam))
+            beforeL = (-(l**2)+cam**2+r**2)/(2*cam*r)
+            beforeR = (l**2+cam**2-r**2)/(2*l*cam)
+            rr.SetVariable("before L", beforeL)
+            rr.SetVariable("before R", beforeR)
+            L = math.acos(beforeL)
+            R = math.acos(beforeR)
 
-    A = math.pi - L - R
-    C = (math.pi-A)/2.0
+            A = math.pi - L - R
+            C = (math.pi-A)/2.0
 
-    error = C-L
-    
-    rr.SetVariable("C", C*180./math.pi)
-    rr.SetVariable("A", A*180./math.pi)
-    rr.SetVariable("Error", error*180./math.pi/2)
-    rr.SetVariable("Visible", True)
-except IndexError:
-    rr.SetVariable("Visible", False)
+            error = C-L
+            
+            #rr.SetVariable("C", C*180./math.pi)
+            #rr.SetVariable("A", A*180./math.pi)
+            rr.SetVariable("TargetAngleError", error*180./math.pi/2)
+            rr.SetVariable("OutsidesVisible", True)
+        else:
+            rr.SetVariable("OutsidesVisible", False)
+    else:
+        rr.SetVariable("InsideVisible", False)
+    rr.SetVariable("TargetVisible", True)
+except TypeError:
+    rr.SetVariable("TargetVisible", False)
+except NameError:
+    rr.SetVariable("TargetVisible", False)
 except ValueError:
-    rr.SetVariable("Visible", False)
+    rr.SetVariable("TargetVisible", False)
